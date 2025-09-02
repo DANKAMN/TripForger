4a3a9a9fc398b92205a14afee1e5179123b41628
DANKAMN <danielnicholas1477@gmail.com>
2025-09-02 14:00:52 +0100

    Update font family

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
       <h2>Hello</h2>
+      <Button>World</Button>
     </div>
   );
 }
