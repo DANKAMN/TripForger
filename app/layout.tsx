86aaf7618289282e832cd0278ab92fd94245bd96
DANKAMN <danielnicholas1477@gmail.com>
2025-09-04 10:45:31 +0100

    Convex DB Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "./provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 
 export const metadata: Metadata = {
@@ -23,9 +23,9 @@ export default function RootLayout({
         <body
           className={inter.className}
         >
-          <Provider>
+          <ConvexClientProvider>
             {children}
-          </Provider>
+          </ConvexClientProvider>
         </body>
       </html>
     </ClerkProvider>
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Header from './_components/Header';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
 
 function Provider ({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>){
+
+  const CreateUser = useMutation(api.user.CreateNewUser)
+
+  const { user } = useUser()
+
+  useEffect(() => {
+    user && CreateNewUser();
+  }, [user])
+
+  const CreateNewUser = async () => {
+    // Save New User
+
+    if (user) {
+      const result = await CreateUser({
+        email: user?.primaryEmailAddress?.emailAddress ?? '',
+        imageUrl: user?.imageUrl,
+        name: user?.fullName ?? ''
+      })
+    }
+    
+    
+  }
 
   return (
     <div>
