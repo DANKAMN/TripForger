e12a508307b715cb4e65c70f95307df466680334
DANKAMN <danielnicholas1477@gmail.com>
2025-09-04 15:13:53 +0100

    Iterate responses

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -42,16 +42,23 @@ const ChatBox = () => {
     <div className="h-[85vh] flex flex-col">
         {/* Display Messages */}
         <section className='flex-1 overflow-y-auto p-4'>
-            <div className="flex justify-end mt-2">
-                <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
-                    User Message
-                </div>
-            </div>
-            <div className="flex justify-start mt-2">
-                <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
-                    AI Agent Message
-                </div>
-            </div>
+            {messages.map((msg:Message, index) => (
+                msg.role == 'user' ? (
+                    <div className="flex justify-end mt-2" key={index}>
+                        <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
+                            {msg.content}
+                        </div>
+                    </div>
+            
+                ) : (
+                    <div className="flex justify-start mt-2" key={index}>
+                        <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
+                            {msg.content}
+                        </div>
+                    </div>
+                )
+            ))}
+            
         </section>
 
         {/* User Input */}
