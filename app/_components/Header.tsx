af3c96bb55ee29ca5d18ef49747921d9a6b18201
DANKAMN <danielnicholas1477@gmail.com>
2025-09-04 10:51:51 +0100

    Change get started button once user is signed in

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { Button } from '@/components/ui/button'
-import { SignInButton } from '@clerk/nextjs'
+import { SignInButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -20,6 +22,8 @@ const menuOptions = [
 ]
 
 const Header = () => {
+    const { user } = useUser()
+
   return (
     <div className='flex justify-between items-center p-4'>
         {/* Logo */}
@@ -38,9 +42,18 @@ const Header = () => {
         </div>
 
         {/* Menu Options */}
-        <SignInButton mode='modal'>
-            <Button>Get Started</Button>
-        </SignInButton>
+        {
+          !user  ? (
+            <SignInButton mode='modal'>
+                <Button>Get Started</Button>
+            </SignInButton>
+          ) : (
+            <Link href={'/create-trip'}>
+                <Button>Forge a Trip</Button>
+            </Link>
+          )
+        }
+        
     </div>
   )
 }
