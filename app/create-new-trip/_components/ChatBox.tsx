34f38d83c58727069a783a8841c049b4334fd95b
DANKAMN <danielnicholas1477@gmail.com>
2025-09-04 15:34:11 +0100

    Placeholder update

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -5,6 +5,7 @@ import { Textarea } from '@/components/ui/textarea'
 import axios from 'axios';
 import { Loader, Send } from 'lucide-react'
 import React, { useState } from 'react'
+import EmptyBoxState from './EmptyBoxState';
 
 type Message = {
     role: string,
@@ -43,6 +44,10 @@ const ChatBox = () => {
 
   return (
     <div className="h-[85vh] flex flex-col">
+        {
+            messages?.length == 0 && <EmptyBoxState />       
+        }
+        
         {/* Display Messages */}
         <section className='flex-1 overflow-y-auto p-4'>
             {messages.map((msg:Message, index) => (
---
+++ app/create-new-trip/_components/EmptyBoxState.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EmptyBoxState = () => {
+  return (
+    <div className='mt7'>
+        <h2 className='font-bold text-2xl text-center'>
+            Forge Your Next <strong className='text-primary'>Adventure</strong> with AI
+        </h2>
+        <p className='mt-3 text-center text-gray-600'>TripForger helps you design personalized trips step by step, turning your ideas into a tailor-made journey with AI.</p>
+    </div>
+  )
+}
+
+export default EmptyBoxState
