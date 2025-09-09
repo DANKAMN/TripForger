043674ba025459e611dfa1facc09da3afc17bb90
DANKAMN <danielnicholas1477@gmail.com>
2025-09-09 20:30:41 +0100

    Fixes for deployment 2

--- app/api/aimodel/route.ts
+++ app/api/aimodel/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from 'openai';
-import { aj } from "../arcjet/route";
+import { aj } from "@/lib/utils";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 const openai = new OpenAI({
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// lib/arcjet.ts
+import arcjet, { tokenBucket } from "@arcjet/next";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    tokenBucket({
+      mode: "LIVE",
+      characteristics: ["userId"],
+      refillRate: 5,
+      interval: 86400,
+      capacity: 10,
+    }),
+  ],
+});
