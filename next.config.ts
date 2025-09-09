1e1eca68db8f350c9f4a9c2720ef09359578a550
DANKAMN <danielnicholas1477@gmail.com>
2025-09-09 21:26:36 +0100

    Fixes for deployment 6

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['places.googleapis.com']
-  }
+    domains: ["places.googleapis.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Prevent client bundle from trying to include server-only modules
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        async_hooks: false, // <-- Ignore async_hooks in client builds
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
