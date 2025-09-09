f302fce4ae19b4d8d15d008b68d77040aeedba52
DANKAMN <danielnicholas1477@gmail.com>
2025-09-09 13:26:19 +0100

    Add floating button

--- app/create-new-trip/page.tsx
+++ app/create-new-trip/page.tsx
@@ -5,6 +5,13 @@ import ChatBox from './_components/ChatBox'
 import Itinerary from './_components/Itinerary'
 import { useTripDetail } from '../provider'
 import GlobalMap from './_components/GlobalMap';
+import { Globe2, Plane } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const CreateNewTrip = () => {
   const { tripDetailInfo, setTripDetailInfo } = useTripDetail()
@@ -19,8 +26,19 @@ const CreateNewTrip = () => {
       <div className="">
           <ChatBox />
       </div>
-      <div className="col-span-2">
+      <div className="col-span-2 relative">
         { activeIndex === 0 ? <Itinerary /> : <GlobalMap /> }
+
+        
+
+        <Tooltip>
+          <TooltipTrigger className='absolute bottom-10 left-[50%]'><Button className='bg-black' size={'lg'}  onClick={() => setActiveIndex(activeIndex === 0 ? 1 : 0)}> 
+              { activeIndex === 0 ? <Plane /> :  <Globe2 /> }
+            </Button></TooltipTrigger>
+          <TooltipContent>
+            Switch between map and Trip
+          </TooltipContent>
+        </Tooltip>
       </div>
     </div>
   )
